3882351b33d763ff37bd3a9b598ecebfed5d15b2
AxisCondo <zubiridoz@hotmail.com>
2026-05-25 19:49:29 +0000

    fix back cambio de condominio

--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -1031,8 +1031,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
-
+    <script>
         document.addEventListener('DOMContentLoaded', function () {
             // Manejador para rotar la flecha de los submenus
             var collapseElements = document.querySelectorAll('.collapse.submenu');
