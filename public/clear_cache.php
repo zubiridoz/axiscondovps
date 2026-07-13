d28566fd198ea7b97a97584a5f607e9af5bd2a4c
AxisCondo <zubiridoz@hotmail.com>
2026-07-13 21:46:47 +0000

    se agrega firma para recibos de pa limpieza

--- public/clear_cache.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (function_exists('opcache_reset')) {
-    opcache_reset();
-    echo "OPcache cleared";
-} else {
-    echo "OPcache not available";
-}
--- public/extract_js.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('app/Views/admin/settings.php', 'utf8');
-const regex = /<script\b[^>]*>([\s\S]*?)<\/script>/gi;
-let match;
-let allScripts = '';
-while ((match = regex.exec(content)) !== null) {
-    allScripts += match[1] + '\n\n';
-}
-fs.writeFileSync('scripts.js', allScripts);
